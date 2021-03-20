3845f787364a2aff2a60c822d7218295a8cdb28e
zevtyardt <apklover76@gmail.com>
2021-03-20 16:14:26 +0700

    Menambahkan beberapa komen

--- bot.py
+++ bot.py
@@ -25,6 +25,8 @@ stopAll = False
 lastSending = None
 
 def sendAlert():
+    # referensi penggunaan global variable
+    # https://stackoverflow.com/questions/4693120/use-of-global-keyword-in-python
     global lastSending
 
     logging.info("sendAlert dijalankan")
@@ -32,13 +34,17 @@ def sendAlert():
         frame = q.get()
         winsound.Beep(2500, 1000)
 
+        # ubah numpy array (frame) ke bentuk format jpg
         success, jpgFrame = cv2.imencode(".jpg", frame)
         if success:
             logging.info(f"Mengirim foto ke {SUPERUSER}")
             ioBuffer = io.BytesIO(frame.read())
+            # referensi: https://stackoverflow.com/a/11696554
             ioBuffer.seek(0)  # penting !!
             bot.send_photo(SUPERUSER, ioBuffer,
                            caption="Terdeteksi tidak menggunakan masker")
+
+            # update timestamp setelah berhasil mengirim foto
             lastSending = time.time()
         q.task_done()
 
@@ -62,15 +68,10 @@ def sendSignal(message):
     stopAll = True
 
 
+# handling ctrl-C signal
 def signal_handler(signal, frame):
     global stopAll
     stopAll = True
-    cv2.release()
-    cv2.destroyAllWindows()
-
-    sys.exit(0)
-
-
 signal.signal(signal.SIGINT, signal_handler)
 
 th = threading.Thread(target=sendAlert)
@@ -92,6 +93,8 @@ logging.info("Memulai kamera")
 cap = cv2.VideoCapture(0)
 
 while not stopAll:
+
+    # deteksi wajah dan lain lain
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
@@ -116,11 +119,17 @@ while not stopAll:
         cv2.putText(frame, pesan, (x, y - 10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
     if mask is False:
+        # setelah terdeteksi tidak menggunakan masker
+        # selanjutnya pastikan kalau waktu terakhir mengirim itu kurang dari DELAY
+        # atau belum diset
         if not lastSending or int(time.time() - lastSending) > DELAY:
-            logging.info("Menambahkan item ke dalam queue")
+            logging.info("Menambahkan frame ke dalam queue")
             q.put(frame)
 
+    # tampilkan frame
     cv2.imshow("frame", frame)
+
+    # referensi: https://stackoverflow.com/a/52913689
     if cv2.waitKey(0) and 0xFF == ord("q"):
         break
 cv2.release()
