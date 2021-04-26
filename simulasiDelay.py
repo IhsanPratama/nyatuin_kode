dcdb3b05be3fbefbf4271535d255263ba98118b0
zevtyardt <apklover76@gmail.com>
2021-04-26 16:52:36 +0700

    Fix delay algorithm

--- simulasiDelay.py
+++ simulasiDelay.py
@@ -3,7 +3,7 @@ import time
 import random
 import queue
 
-interval = 5
+interval = 2
 stopAll = False
 q = queue.Queue()
 
@@ -14,28 +14,28 @@ is_not_sending = False
 def sendAlert(thread):
     global timestamp, is_not_sending
 
+    skipped = 0
     while not stopAll:
-        i, q.get()
+        i = q.get()
 
         sec = int(time.time() - timestamp)
-        if sec >= interval and is_not_sending:
-            with lock:
-                timestamp = time.time()
-            print ("\n--- [thread-%s] processed [seconds=%s, interval=%s, is_not_sending=%s]" % (thread,
-                sec, interval, is_not_sending
+        if sec > interval:
+            print ("\n--- [thread-%s] processed [seconds=%s, interval=%s, skipped=%s]" % (thread,
+                sec, interval, skipped
             ))
             time.sleep(random.randrange(5))
+            with lock:
+                timestamp = time.time()
+            skipped = 0
         else:
             print("[thread-%s] waiting [seconds=%s] [%s]" % (thread, sec, i), end="\r")
-            with lock:
-                is_not_sending = not is_not_sending
             time.sleep(random.random())
+            skipped += 1
         q.task_done()
 
-for i in range(1):
-    th = threading.Thread(target=sendAlert, args=(i, ))
-    th.setDaemon(True)
-    th.start()
+th = threading.Thread(target=sendAlert, args=("1", ))
+th.setDaemon(True)
+th.start()
 
 for i in range(100000000000):
     q.put(i)
