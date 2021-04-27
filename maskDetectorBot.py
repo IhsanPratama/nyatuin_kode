2ee01ceb3c7e7e3bab5482681c53c7800f747ccd
zevtyardt <apklover76@gmail.com>
2021-04-27 12:54:58 +0700

    Separates the detectmask function into multiple threads

--- maskDetectorBot.py
+++ maskDetectorBot.py
@@ -4,15 +4,15 @@ logging.basicConfig(
 
 logging.info("Mengimport module")
 
-import sys
-import signal
-import cv2
-import telebot
-import threading
-import queue
-import io
-import time
 import simpleaudio as sa
+import time
+import io
+import queue
+import threading
+import telebot
+import cv2
+import signal
+import sys
 
 logging.info("Inisiasi Bot telegram")
 bot = telebot.TeleBot(
@@ -20,8 +20,32 @@ bot = telebot.TeleBot(
 )
 SUPERUSER = 626351605  # ganti pakai id telegrammu
 
-q = queue.Queue()
+# semua frame akan disimpan disini
+frame_tanpa_masker = queue.Queue()
+raw_frame = queue.Queue()
+
+jumlahThreadDetectMask = 3
+
 stopAll = False
+
+frame_start_time = time.time()
+frame_counter = 0
+FPS = 0
+
+fontFace = cv2.FONT_HERSHEY_SIMPLEX
+
+# posisi text / koordinat
+coordinate = (10, 30)
+
+# Skala font / ukuran font
+# rumus: ukuran font asli dikali n
+fontScale = 1
+
+# warna font: putih
+fontColor = (255, 255, 255)
+
+# tipe line
+lineType = 2
 
 # interval = delay
 interval = 2
@@ -53,7 +77,7 @@ def sendAlert():
 
     total_dilewati = 0
     while not stopAll:
-        frame = q.get()
+        frame = frame_tanpa_masker.get()
 
         sec = int(time.time() - timestamp)
         if sec > interval:
@@ -75,7 +99,46 @@ def sendAlert():
                     total_dilewati = 0
         else:
             total_dilewati += 1
-        q.task_done()
+        frame_tanpa_masker.task_done()
+
+
+logging.info("Inisisasi HaarCascadeClasifier")
+face = cv2.CascadeClassifier(
+    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+smile = cv2.CascadeClassifier(
+    cv2.data.haarcascades + 'haarcascade_smile.xml')
+
+def detectMask():
+    while not stopAll:
+        raw = raw_frame.get()
+
+        frame = cv2.flip(raw, 1)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+        faces = face.detectMultiScale(gray, 1.1, 5, 0, (140, 140), (250, 250))
+
+        mask = True
+        for x, y, w, h in faces:
+            smiles = smile.detectMultiScale(
+                gray[y:y + h, x:x + w], 1.4, 5, 0, (75, 75), (90, 90))
+            pesan = 'Dengan Masker'
+            color = (0, 255, 0)
+
+            for ex, ey, ew, eh in smiles:
+                logging.debug("Terdeteksi tanpa masker")
+                pesan = 'Tanpa Masker'
+                color = (0, 0, 255)
+                mask = False
+                break
+
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(frame, pesan, (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+
+        # lewati frame jika thread dalam kondisi lock tidak peduli mask
+        # bernilai True
+        if mask is True and not lock.locked():
+            frame_tanpa_masker.put(frame)
+        raw_frame.task_done()
 
 
 @bot.message_handler(commands=['start'])
@@ -97,115 +160,60 @@ def sendSignal(message):
     stopAll = True
 
 
-def remove_item_queue():
-    while not q.empty():
-        try:
-            q.get(timeout=1)
-        except Exception:
-            pass
-
-
 def cleanup(*args):
     global stopAll
 
     stopAll = True
     cv2.release()
     cv2.destroyAllWindows()
-    remove_item_queue()
 
     sys.exit(0)
 
 
 signal.signal(signal.SIGINT, cleanup)
 
+# Thread Process
 th = threading.Thread(target=sendAlert)
 th.setDaemon(True)
 th.start()
+
+for _ in range(jumlahThreadDetectMask):
+    th = threading.Thread(target=detectMask)
+    th.setDaemon(True)
+    th.start()
 
 logging.info("Bot Berjalan dilatar belakang")
 th = threading.Thread(target=bot.polling)
 th.setDaemon(True)
 th.start()
 
-logging.info("Inisisasi CascadeClasifier")
-face = cv2.CascadeClassifier(
-    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-smile = cv2.CascadeClassifier(
-    cv2.data.haarcascades + 'haarcascade_smile.xml')
+# =======
 
 logging.info("Memulai kamera")
 cap = cv2.VideoCapture(0)
 
-frame_start_time = time.time()
-frame_counter = 0
-FPS = 0
-
-# list default font:
-#   FONT_HERSHEY_COMPLEX
-#   FONT_HERSHEY_COMPLEX_SMALL
-#   FONT_HERSHEY_DUPLEX
-#   FONT_HERSHEY_PLAIN
-#   FONT_HERSHEY_SCRIPT_COMPLEX
-#   FONT_HERSHEY_SCRIPT_SIMPLEX
-#   FONT_HERSHEY_SIMPLEX
-#   FONT_HERSHEY_TRIPLEX
-#   FONT_ITALIC
-fontFace = cv2.FONT_HERSHEY_SIMPLEX
-
-# posisi text / koordinat
-coordinate = (10, 20)
-
-# Skala font / ukuran font
-# rumus: ukuran font asli dikali n
-fontScale = 1
-
-# warna font: putih
-fontColor = (255, 255, 255)
-
-# tipe line
-lineType = 2
-
 while not stopAll:
     ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-    faces = face.detectMultiScale(gray, 1.1, 5, 0, (140, 140), (250, 250))
+    if not ret:
+        break
 
-    mask = True
-    for x, y, w, h in faces:
-        smiles = smile.detectMultiScale(
-            gray[y:y + h, x:x + w], 1.4, 5, 0, (75, 75), (90, 90))
-        pesan = 'Dengan Masker'
-        color = (0, 255, 0)
-
-        for ex, ey, ew, eh in smiles:
-            logging.debug("Terdeteksi tanpa masker")
-            pesan = 'Tanpa Masker'
-            color = (0, 0, 255)
-            mask = False
-            break
-
-        # aku ubah gray jadi frame soalnya yg dikirim gambar abu2
-        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-        cv2.putText(frame, pesan, (x, y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
-    if mask is False and not lock.locked():
-        q.put(frame)
+    # lewati frame jika thread dalam kondisi lock (frame sedang
+    # dikirim keclient telegram)
+    if not lock.locked():
+        raw_frame.put(frame)
 
     # tambah 1 setiap frame yang berhasil diambil
     frame_counter += 1
 
     # hitung durasi
     elapsed_time = time.time() - frame_start_time
-    if (elapsed_time) > 1:
+    if elapsed_time > 1:
         # rumus: jumlah frame dibagi durasi
-        FPS = str(int(frame_counter / elapsed_time))
+        FPS = int(frame_counter / elapsed_time)
 
         # XXX: harusnya dapet fps lebih dari 50, soalnya frame diproses dilatar belakang
         # lupa kalau didalem loop masih ada detectMultiScale, jadi kisaran fps yg didapet
         # sekitar 15-30 mungkin lebih
-
-        # logging.info("FPS: %s", fps)
 
         # reset
         frame_counter = 0
@@ -222,11 +230,8 @@ while not stopAll:
         lineType=lineType
     )
 
-    logging.debug("Queue size: %s", q.qsize())
-
     cv2.imshow("frame", frame)
     if cv2.waitKey(1) and 0xFF == ord("q"):
         break
-
 
 cleanup()
